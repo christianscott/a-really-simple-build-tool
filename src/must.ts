992f945d686d57fab3a57d043359414ff27f860f
Christian Scott <christian.s@canva.com>
2023-08-31 19:34:55 +1000

    remve TS impl

--- src/must.ts
+++
@@ -1,9 +0,0 @@
-import { strict as assert } from "assert";
-
-export function must<T>(
-	x: T | undefined | null,
-	msg: string = "x does not exist",
-): T {
-	assert(x != null, msg);
-	return x;
-}
